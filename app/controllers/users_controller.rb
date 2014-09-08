0f9db8f67cb7b81335515768a8a5c36cd93a6f93
Ryoma Ono <ryoma.ono.2661@gmail.com>
2014-09-08 14:47:12 +0900

    [Exercises 9.6.6] サインインユーザーが不要なアクションを実行したときルートURLにリダイレクトする

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
-  before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
-  before_action :correct_user,   only: [:edit, :update]
-  before_action :admin_user,     only: :destroy
+  before_action :signed_in_user,  only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,    only: [:edit, :update]
+  before_action :admin_user,      only: :destroy
+  before_action :redirected_user, only: [:new, :create]
 
   def index
     @users = User.paginate(page: params[:page])
@@ -67,5 +68,9 @@ class UsersController < ApplicationController
 
     def admin_user
       redirect_to(root_url) unless current_user.admin?
+    end
+
+    def redirected_user
+      redirect_to root_url if signed_in?
     end
 end

85cfee5bbfb1f71dd49ebea20ccac83f8c4b41d9
Ryoma Ono <ryoma.ono.2661@gmail.com>
2014-09-09 12:22:16 +0900

    [Exercises 9.6.8] フレンドリーフォワーディングが初回サインインにのみ有効となっているかテストする

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -54,8 +54,10 @@ class UsersController < ApplicationController
    # Before filters
 
     def signed_in_user
-      store_location
-      redirect_to signin_url, notice: "Please sign in." unless signed_in?
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
     end
 
     def correct_user

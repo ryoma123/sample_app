899db5c5c10263d5e27a46cce12f2676c7d3e109
Ryoma Ono <ryoma.ono.2661@gmail.com>
2014-09-10 14:28:34 +0900

    [Exercises 9.6.9] Adminユーザーが自身のアカウントを削除できないようにdestroyアクションを修正する

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -117,5 +117,15 @@ describe "Authentication" do
         specify { expect(response).to redirect_to(root_url) }
       end
     end
+
+    describe "as admin user" do
+      let(:admin) { FactoryGirl.create(:admin) }
+      before { sign_in admin, no_capybara: true }
+
+      describe "submitting a DELETE request to the Admin Users#destroy action" do
+        before { delete user_path(admin) }
+        specify { expect(response).to redirect_to(users_url) }
+      end
+    end
   end
 end
